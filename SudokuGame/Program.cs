29dd801683e54aef6706d273f4e5e3ea6dea598f
nicole <nicole.a.jordaan@gmail.com>
2023-02-20 16:10:46 +0000

    Brought project up to date

--- SudokuGame/Program.cs
+++ SudokuGame/Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+
+namespace SudokuGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int[,] numbers = new int[3, 3];
+
+            for (int i = 0; i < numbers.GetLength(0); ++i)
+            {
+                for (int j = 0; j < numbers.GetLength(1); ++j)
+                {
+                    numbers[0, j] = j;
+
+                    Console.Write($" | {numbers[0, j]} |");
+                }
+                numbers[i, 0] = i;
+
+                Console.WriteLine("\n");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Random()
+        {
+            //Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            //int[] num = new int[9];
+            //int[] prevNums = new int[20];
+
+            //int prevNum = 10;
+            //int randNum;
+            //Random rnd = new Random();
+
+            //Console.WriteLine(num.Length);
+
+            //for (int i = 0; i < num.Length; i++)
+            //{
+            //    do
+            //    {
+            //        Console.Write(".");
+            //       randNum = rnd.Next(1, 10);
+
+            //    }
+            //    while (num.Contains(randNum));
+            //    num[i] = randNum;
+            //    Console.WriteLine($"\r\nNUM {i} : {num[i]}");
+
+            //}
+        }
+
+
+    }
+
+}
