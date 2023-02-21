7992bdf0b1073dcc2eaef8061050b035fbfe9642
nicole <nicole.a.jordaan@gmail.com>
2023-02-21 20:30:54 +0000

    Board Created

--- SudokuGame/Program.cs
+++ SudokuGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.Serialization.Formatters;
 
 namespace SudokuGame
@@ -7,26 +8,140 @@ namespace SudokuGame
     {
         static void Main(string[] args)
         {
+            //Console.ForegroundColor= ConsoleColor.Green;
+
+            /*    i = inner    o = outer     */
+
+            // width & height of inner block
+            int iw = 3;
+            int ih = 3;
+
+            // width & height of outer block
+            int ow = 3; 
+            int oh = 3;
+
+            int[,,,] board = new int[ow, oh, iw, ih];
 
 
+            DrawSqaure(board);
 
-            int[,] numbers = new int[3, 3];
+        }
 
-            for (int i = 0; i < numbers.GetLength(0); ++i)
+        static void DrawSqaure(int[,,,] board)
+        {
+            // width & height of outer block
+            int ow = board.GetLength(0);
+            int oh = board.GetLength(1);
+            int iw = board.GetLength(2);
+            int ih = board.GetLength(3);
+
+            for (int oy = 0; oy < oh; oy++) // y coords of outer block
             {
-                for (int j = 0; j < numbers.GetLength(1); ++j)
+                for (int iy = 0; iy < ih; iy++) // y coords of inner block
                 {
-                    numbers[0, j] = j;
+                    Console.Write(" "); // after each inner y
+                    for (int ox = 0; ox < ow; ox++) // x coords of outer block
+                    {
 
-                    Console.Write($" | {numbers[0, j]} |");
+                        for (int ix = 0; ix < iw; ix++) // x coords of inner block
+                        {
+                            Console.Write($" {board[ox,oy,ix,iy]} ");
+                        }
+
+                        Console.Write(" ");
+                    }
+
+                    Console.WriteLine(); // end of inner line
                 }
-                numbers[i, 0] = i;
+                //Console.WriteLine(new String('-', (oh * ih) * 4 + 1 ));
+                Console.WriteLine();
+            }
+
+
+
+        }
+
+
+
+
+
+
+
+        static int[,] SmallSquare()
+        {
+            int[,] array = new int[3, 3];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                array[i, 0] = i;
+                //Console.WriteLine(array[i, 0]);
+
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[0, j] = j;
+                    Console.Write(" | " + array[0, j] + " | ");
+
+
+                }
 
                 Console.WriteLine("\n");
             }
 
-            Console.ReadLine();
+            return array;
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //int[,] numbers = new int[3, 3];
+
+        //for (int i = 0; i < numbers.GetLength(0); ++i)
+        //{
+        //    for (int j = 0; j < numbers.GetLength(1); ++j)
+        //    {
+        //        numbers[0, j] = j;
+
+        //        Console.Write($" | {numbers[0, j]} |");
+        //    }
+        //    numbers[i, 0] = i;
+
+        //    Console.WriteLine("\n");
+        //}
+
+        //Console.ReadLine();
+
 
         static void Random()
         {
@@ -55,8 +170,6 @@ namespace SudokuGame
 
             //}
         }
-
-
     }
-
 }
+
