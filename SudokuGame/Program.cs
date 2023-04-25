de26427a5b1dcfe1a01e6710c59e829bd5f3fc6f
nicole <nicole.a.jordaan@gmail.com>
2023-04-25 19:14:10 +0100

    Generate sudoku board and remove spaces

--- SudokuGame/Program.cs
+++ SudokuGame/Program.cs
@@ -18,14 +18,18 @@ namespace SudokuGame {
             int oh = 3;
 
             int[,,,] board = new int[ow, oh, iw, ih];
+            
 
             GeneratePuzzle(board);
             DrawSqaure(board);
 
-            Console.WriteLine();
-            //SmallSquare();
-            //Random();
+
+            Console.WriteLine(board.Length);
         }
+
+
+
+       
 
 
         // Displaying the Grid
@@ -49,8 +53,23 @@ namespace SudokuGame {
 
                         for (int ix = 0; ix < iw; ix++) // x coords of inner block
                         {
+                            if (board[ox, oy, ix, iy] == 0)
+                            {
+                                Console.ForegroundColor= ConsoleColor.Green;
+                                Console.Write($" {board[ox, oy, ix, iy]} ");
+                                Console.ForegroundColor = ConsoleColor.White;
+
+
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.White;
+                                Console.Write($" {board[ox, oy, ix, iy]} ");
+
+                            }
+
                             // board[ox, oy, ix, iy] = ix;
-                            Console.Write($" {board[ox, oy, ix, iy]} ");
+                            //Console.Write($" {board[ox, oy, ix, iy]} ");
                         }
 
                         Console.Write(" |");
@@ -77,160 +96,291 @@ namespace SudokuGame {
             return array;
         }
 
-
-
-
-        // Generates a sudoku puzzle using backtracking algorithm
-        static void GeneratePuzzle(int[,,,] board) {
+        static void DeleteSpace(int[,,,] board)
+        {
             int ow = board.GetLength(0);
             int oh = board.GetLength(1);
             int iw = board.GetLength(2);
             int ih = board.GetLength(3);
 
-
             Random rnd = new Random();
+            int randNum;
 
-            HashSet<int> usedNums = new HashSet<int>();
+            // number of spaces to remove (difficulty)
+            int numToRemove = 10;
 
 
-            // top row of the puzzle
-
-            for (int oy = 0; oy < oh; oy++) // y coords of outer block
+            for (int i = 0; i <= 70; i++)
             {
-                for (int ox = 0; ox < ow; ox++) // x coords of outer block
-                {
-                    Console.WriteLine($"ox {ox} oy {oy}");
-                    var isOk = false;
-                    var retryCount = 0;
-                    while (!isOk) {
-                        //retryCount++;
-                        //if (retryCount == 1) {
-                        //    if (ox > 0) ox--;
-                        //    Console.WriteLine($"Decrease ox {ox}");
+                
+                int ox = rnd.Next(0,3);
+                int oy = rnd.Next(0,3);
+                int ix = rnd.Next(0,3);
+                int iy = rnd.Next(0,3);
 
-                        //} else if (retryCount == 2) {
-                        //    if (oy > 0) oy--;
-                        //    Console.WriteLine($"Decrease oy {oy}");
-                        //    retryCount = 0;
-                        //}
-
-                        DrawSqaure(board);
-                        Console.Clear();
-                        isOk = true;
-                        for (int iy = 0; iy < ih; iy++) // y coords of inner block
-                        {
-                            var randSet = getRandomSet();
-
-                            for (int ix = 0; ix < iw; ix++) // x coords of inner block
-                            {
-                                for (var cnt = 0; cnt < 9; cnt++) {
-                                    var randNum = randSet[cnt];
-
-                                    if (isValid(board, ox, oy, ix, iy, randNum)) {
-                                        board[ox, oy, ix, iy] = randNum;
-                                        Console.WriteLine($"{ox},{oy},{ix},{iy}  ??? {randNum}");
-                                        break;
-
-                                    }
-                                    if (cnt == 8) {
-                                        isOk = false;
-                                        Console.WriteLine("RETRY...");
-                                    }
-                                }
-
-                            }
-
-                        }
-                    }
-
-                }
-
+                board[ox, oy, ix, iy] = 0;
             }
+
+
 
         }
 
 
-        static bool isValid(int[,,,] board, int outerCol, int outerRow, int innerCol, int innerRow, int num) {
+        static void GeneratePuzzle(int[,,,] board)
+        {
             int ow = board.GetLength(0);
             int oh = board.GetLength(1);
             int iw = board.GetLength(2);
             int ih = board.GetLength(3);
 
-            //col
-            for (int ox = 0; ox < ow; ox++) {
-                for (int ix = 0; ix < iw; ix++) {
-                    if (board[ox, outerRow, ix, innerRow] == num) {
-                        return false;
+            // top row
+            HashSet<int> usedNums = new HashSet<int>();
+            Random random = new Random();
+
+            int randNum;
+
+            for (int ox = 0; ox < ow; ox++)
+            {
+                for (int ix = 0; ix < iw; ix++)
+                {
+                    do
+                    {
+                        randNum = random.Next(1, 10);
+
+                    }
+                    while (usedNums.Contains(randNum));
+
+                    usedNums.Add(randNum);
+                    board[ox, 0, ix, 0] = randNum;
+                }
+            }
+
+
+            Solve(board);
+
+            DeleteSpace(board);
+
+
+
+        }
+
+
+        static bool Solve(int[,,,] board)
+        {
+            int ow = board.GetLength(0);
+            int oh = board.GetLength(1);
+            int iw = board.GetLength(2);
+            int ih = board.GetLength(3);
+
+            Random rnd = new Random();
+            int randNum;
+
+            for (int ox = 0; ox < ow; ox++) // y coords of outer block
+            {
+                for (int oy = 0; oy < oh; oy++) // y coords of inner block
+                {
+                    for (int ix = 0; ix < iw; ix++) // x coords of outer block
+                    {
+                        for (int iy = 0; iy < ih; iy++) // x coords of inner block
+                        {
+                            if (board[ox, oy, ix, iy] == 0) // empty space
+                            {
+                                for (int num = 1; num <= 9; num++)
+                                {
+                                    if (isValid(board, ox, oy, ix, iy, num))
+                                    {
+                                        board[ox, oy, ix, iy] = num;
+
+                                        if (Solve(board))
+                                        {
+                                            return true;
+                                        }
+
+                                        board[ox, oy, ix, iy] = 0;
+                                    }
+                                }
+
+                                return false;
+                            }
+                        }
 
                     }
                 }
             }
 
-            //row
-            for (int oy = 0; oy < oh; oy++) {
-                for (int iy = 0; iy < ih; iy++) {
-                    if (board[outerCol, oy, innerCol, iy] == num) {
-                        return false;
-
-                    }
-                }
-            }
-
-            // inner square
-            for (int iy = 0; iy < ih; iy++) {
-                for (int ix = 0; ix < iw; ix++) {
-                    //Console.WriteLine($"{outerCol},{outerRow},{ix},{iy}:{board[outerCol, outerRow, ix, iy]}  ??? {num}");
-                    //Console.WriteLine($"{outerCol},{outerRow},{ix},{iy}: {board[outerCol, outerRow, ix, iy]}  ??? {num}");
-                    if (board[outerCol, outerRow, ix, iy] == num) {
-                        return false;
-                    }
-                }
-            }
 
             return true;
         }
 
 
 
-        //static bool isValid_OLD(int[,,,] board, int outerRow, int outerCol, int innerRow, int innerCol, int num) {
-        //    int ow = board.GetLength(0);
-        //    int oh = board.GetLength(1);
-        //    int iw = board.GetLength(2);
-        //    int ih = board.GetLength(3);
+
+
+            // Generates a sudoku puzzle using backtracking algorithm
+            static void GeneratePuzzle_OLD(int[,,,] board)
+            {
+                int ow = board.GetLength(0);
+                int oh = board.GetLength(1);
+                int iw = board.GetLength(2);
+                int ih = board.GetLength(3);
+
+
+                Random rnd = new Random();
+
+                HashSet<int> usedNums = new HashSet<int>();
+
+
+                // top row of the puzzle
+
+                for (int oy = 0; oy < oh; oy++) // y coords of outer block
+                {
+                    for (int ox = 0; ox < ow; ox++) // x coords of outer block
+                    {
+                        Console.WriteLine($"ox {ox} oy {oy}");
+                        var isOk = false;
+                        var retryCount = 0;
+                        while (!isOk)
+                        {
+                            //retryCount++;
+                            //if (retryCount == 1) {
+                            //    if (ox > 0) ox--;
+                            //    Console.WriteLine($"Decrease ox {ox}");
+
+                            //} else if (retryCount == 2) {
+                            //    if (oy > 0) oy--;
+                            //    Console.WriteLine($"Decrease oy {oy}");
+                            //    retryCount = 0;
+                            //}
+
+                            // DrawSqaure(board);
+                            //  Console.Clear();
+                            isOk = true;
+                            for (int iy = 0; iy < ih; iy++) // y coords of inner block
+                            {
+                                var randSet = getRandomSet();
+
+                                for (int ix = 0; ix < iw; ix++) // x coords of inner block
+                                {
+                                    for (var cnt = 0; cnt < 9; cnt++)
+                                    {
+                                        var randNum = randSet[cnt];
+
+                                        if (isValid(board, ox, oy, ix, iy, randNum))
+                                        {
+                                            board[ox, oy, ix, iy] = randNum;
+                                            Console.WriteLine($"{ox},{oy},{ix},{iy}  ??? {randNum}");
+                                            break;
+
+                                        }
+                                        //if (cnt == 8) {
+                                        //    isOk = false;
+                                        //    Console.WriteLine("RETRY...");
+                                        //}
+                                    }
+
+                                }
+
+                            }
+                        }
+
+                    }
+
+                }
+
+            }
+
+
+            // Checks if number is present in row, col or inner 3x3 square
+            static bool isValid(int[,,,] board, int outerCol, int outerRow, int innerCol, int innerRow, int num)
+            {
+                int ow = board.GetLength(0);
+                int oh = board.GetLength(1);
+                int iw = board.GetLength(2);
+                int ih = board.GetLength(3);
+
+                //col
+                for (int ox = 0; ox < ow; ox++)
+                {
+                    for (int ix = 0; ix < iw; ix++)
+                    {
+                        if (board[ox, outerRow, ix, innerRow] == num)
+                        {
+                            return false;
+
+                        }
+                    }
+                }
+
+                //row
+                for (int oy = 0; oy < oh; oy++)
+                {
+                    for (int iy = 0; iy < ih; iy++)
+                    {
+                        if (board[outerCol, oy, innerCol, iy] == num)
+                        {
+                            return false;
+
+                        }
+                    }
+                }
+
+                // inner square
+                for (int iy = 0; iy < ih; iy++)
+                {
+                    for (int ix = 0; ix < iw; ix++)
+                    {
+                        //Console.WriteLine($"{outerCol},{outerRow},{ix},{iy}:{board[outerCol, outerRow, ix, iy]}  ??? {num}");
+                        //Console.WriteLine($"{outerCol},{outerRow},{ix},{iy}: {board[outerCol, outerRow, ix, iy]}  ??? {num}");
+                        if (board[outerCol, outerRow, ix, iy] == num)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                return true;
+            }
 
 
 
-        //    // row
-        //    for (int ox = 0; ox < ow; ox++) {
-        //        for (int ix = 0; ix < iw; ix++) {
-        //            if (board[ox, outerRow, ix, innerRow] == num) {
-        //                return false;
+            //static bool isValid_OLD(int[,,,] board, int outerRow, int outerCol, int innerRow, int innerCol, int num) {
+            //    int ow = board.GetLength(0);
+            //    int oh = board.GetLength(1);
+            //    int iw = board.GetLength(2);
+            //    int ih = board.GetLength(3);
 
-        //            }
-        //        }
-        //    }
 
-        //    // col
-        //    for (int oy = 0; oy < oh; oy++) {
-        //        for (int iy = 0; iy < ih; iy++) {
-        //            if (board[outerCol, oy, innerCol, iy] == num) {
-        //                return false;
 
-        //            }
-        //        }
-        //    }
+            //    // row
+            //    for (int ox = 0; ox < ow; ox++) {
+            //        for (int ix = 0; ix < iw; ix++) {
+            //            if (board[ox, outerRow, ix, innerRow] == num) {
+            //                return false;
 
-        //    // inner square
-        //    for (int ix = 0; ix < iw; ix++) {
-        //        for (int iy = 0; iy < ih; iy++) {
-        //            if (board[outerCol, outerRow, ix, iy] == num) {
-        //                return false;
-        //            }
-        //        }
-        //    }
-        //    return true;
-        //}
+            //            }
+            //        }
+            //    }
 
+            //    // col
+            //    for (int oy = 0; oy < oh; oy++) {
+            //        for (int iy = 0; iy < ih; iy++) {
+            //            if (board[outerCol, oy, innerCol, iy] == num) {
+            //                return false;
+
+            //            }
+            //        }
+            //    }
+
+            //    // inner square
+            //    for (int ix = 0; ix < iw; ix++) {
+            //        for (int iy = 0; iy < ih; iy++) {
+            //            if (board[outerCol, outerRow, ix, iy] == num) {
+            //                return false;
+            //            }
+            //        }
+            //    }
+            //    return true;
+            //}
 
 
 
